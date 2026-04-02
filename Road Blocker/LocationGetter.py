d03a26ae4bf7d2756365603132b400d60b60c7d0
JaysonC <jaysonc678@gmail.com>
2026-04-02 13:38:58 -0600

    Added searching by address

--- Road Blocker/LocationGetter.py
+++ Road Blocker/LocationGetter.py
@@ -5,26 +5,44 @@ import math
 class LocationGetter:
 
     def get_public_ip(self):
+        #Gets IP
         response = requests.get("http://checkip.dyndns.com/")
         response.raise_for_status()
         data = response.text
+        #Uses regex to parse response text for ip address
         ip_address = re.search(r"Address: (\d+\.\d+\.\d+\.\d+)", data)
         if not ip_address:
             raise ValueError("Could not parse public IP from response")
         return ip_address.group(1)
 
     def get_location_from_ip(self,api_key, ip_address):
+        #Uses ip address to get location
         url = f"https://ipinfo.io/{ip_address}/json?token={api_key}"
         response = requests.get(url)
         return response.json()
 
-    def get_location_coordinates(self):
+    def get_location_coordinates_from_ip(self):
+        #Gets ip
         api_key = "477233b96ca693"
         ip_address = self.get_public_ip()
+        #Then gets coordinates from ip
         location_data = self.get_location_from_ip(api_key, ip_address)
-        return 40.7132, -74.0061
+        return location_data["loc"].split(",")
+    
+    def get_location_coordinates_from_address(self, address):
+        #Make an api call to tomtom for geocoding the address
+        api_key = "zTfX7b0hg5V9N5Jzi0bngmq1lFL7vmms"
+        url = f"https://api.tomtom.com/search/2/geocode/{address}.json?key={api_key}"
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        if data["results"] is None:
+            raise RuntimeError("No results found")
+        location = data["results"][0]["position"]
+        return [location["lat"], location["lon"]]
     
     def convert_location_to_tile_data(self, lat, lon, zoom):
+        #Converts coordinates to tiles system. Annoying sphere math
         MIN_ZOOM = 0
         MAX_ZOOM = 22
         MIN_LAT = -85.051128779807
@@ -49,4 +67,4 @@ class LocationGetter:
         
 if __name__ == "__main__":
     lg = LocationGetter()
-    print(lg.get_location_coordinates())
+    print(lg.get_location_coordinates_from_ip())
--- Road Blocker/TrafficGetter.py
+++ Road Blocker/TrafficGetter.py
@@ -36,16 +36,13 @@ class TrafficGetter:
         
 if __name__ == "__main__":
     lg = LocationGetter()
-    location = lg.get_location_coordinates()
+    location = lg.get_location_coordinates_from_address("Alamogordo, New Mexico")
     lat, lon = float(location[0]), float(location[1])
     tg = TrafficGetter()
     api_key = "zTfX7b0hg5V9N5Jzi0bngmq1lFL7vmms"
-    zoom, x, y = lg.convert_location_to_tile_data(lat,lon, 12)
+    zoom, x, y = lg.convert_location_to_tile_data(lat,lon, 11)
     tg.save_traffic_image_from_x_y_to_file(api_key, x, y, zoom)
     tg.show_traffic_image()
     tg.save_traffic_data_from_coords_to_file(api_key, lat, lon, zoom)
     current_speed, free_flow_speed = tg.get_simple_traffic_data_from_file()
     print(f"Current speed in your area is {current_speed}mph, which is {free_flow_speed- current_speed} mph less than the free flow speed of {free_flow_speed}mph")
-    
-    
-    
