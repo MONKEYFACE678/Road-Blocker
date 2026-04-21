03c1135914b7b5dddd7f1e3e13051140c07e8061
soggynachos <angel13asdf@gmail.com>
2026-04-21 14:56:18 -0600

    added info to settings window file still needs to be functional

--- RoadBlocker/Main.py
+++ RoadBlocker/Main.py
@@ -1,15 +1,4 @@
-from tkinter import *
-import PIL.Image as im
-import PIL.ImageTk as imtk
-from PIL import Image
-import os as os
-import LocationGetter 
-import TrafficGetter 
-import traffic_data_window
-import Menu_Window
-import About_Us_Window
-import Help_Window
-import Setting_Window
+
 import start_window
 
 if __name__ == "__main__":
--- RoadBlocker/Setting_Window.py
+++ RoadBlocker/Setting_Window.py
@@ -16,4 +16,17 @@ import start_window
 class settings_window(Toplevel):
      def __init__(self):
         super().__init__()
-        self.geometry("400x500")
+        self.geometry("1000x500")
+    
+     
+        stats = Label(self, text="Stats Setting", font=("Arial", 16))
+        stats.place(x=20, y=50)
+        density = Label(self, text="Traffic Density:", font=("Arial", 12))
+        density.place(x=20, y=75)
+        density_var = StringVar(value="Medium")
+        
+        option_menu = OptionMenu(self, density_var, "Low", "Medium", "High")
+        option_menu.place(x=20, y=100)
+
+        save = Button(self, text="Save Settings", command=lambda: print(f" Density: {self.density_var.get()}"))
+        save.place(x=20, y=150)
